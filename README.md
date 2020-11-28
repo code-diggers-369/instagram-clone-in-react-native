47732d8a94e1ee02605bbf4382a1cdd2b3bdb5d6
code-diggers-369 <codediggers007@gmail.com>
2020-11-28 10:41:28 +0530

    add android

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # instagram-clone-in-react-native
+
 Instagram clone maked in react native with firebase as realtime database in this project. you can upload photo and give comment. make new accounts. etc are included in this
+
+change config/config.js put firebase config here replace const config with your
 
 #instagram clone screen shots
 
--- config/config.js
+++ config/config.js
@@ -1,21 +1,33 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 //api details
+
+///change this config
 ///change this config
 const config = {
-    apiKey: "asdlasaskjaskjkasjckajscjaskljdasd",
-    authDomain: "dasd.das.firebaseapp.com",
-    databaseURL: "https://dasdadasdad.firebaseio.com",
-    projectId: "safasfsa",
-    storageBucket: "dsadasdasd.appspot.com",
-    messagingSenderId: "264295232442476",
-    appId: "1:2632432444:web:3668e05f9fsdfsdf34",
-    measurementId: "G-JK79J"
-  };
+  apiKey: "AIzaSyCCCh3G4iT8ATIO58ialolr4LTWm6wWp5Y",
+  authDomain: "digi-club.firebaseapp.com",
+  databaseURL: "https://digi-club.firebaseio.com",
+  projectId: "digi-club",
+  storageBucket: "digi-club.appspot.com",
+  messagingSenderId: "269545774676",
+  appId: "1:269545774676:web:3668e05f9a2c84b57957c1",
+  measurementId: "G-XZ0091GE0S",
+};
+// const config = {
+//   apiKey: "asdlasaskjaskjkasjckajscjaskljdasd",
+//   authDomain: "dasd.das.firebaseapp.com",
+//   databaseURL: "https://dasdadasdad.firebaseio.com",
+//   projectId: "safasfsa",
+//   storageBucket: "dsadasdasd.appspot.com",
+//   messagingSenderId: "264295232442476",
+//   appId: "1:2632432444:web:3668e05f9fsdfsdf34",
+//   measurementId: "G-JK79J",
+// };
 
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-    export const f = firebase;
-    export const database = firebase.database();
-    export const auth = firebase.auth();
-    export const storage = firebase.storage();
+export const f = firebase;
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
