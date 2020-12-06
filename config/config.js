bc61d972e9666f12762f5c305c442f59e24eca4f
Code Diggers <63188749+code-diggers-369@users.noreply.github.com>
2020-12-06 20:33:55 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,26 +4,17 @@ import firebase from "firebase";
 
 ///change this config
 ///change this config
+
 const config = {
-  apiKey: "AIzaSyCCCh3G4iT8ATIO58ialolr4LTWm6wWp5Y",
-  authDomain: "digi-club.firebaseapp.com",
-  databaseURL: "https://digi-club.firebaseio.com",
-  projectId: "digi-club",
-  storageBucket: "digi-club.appspot.com",
-  messagingSenderId: "269545774676",
-  appId: "1:269545774676:web:3668e05f9a2c84b57957c1",
-  measurementId: "G-XZ0091GE0S",
+  apiKey: "asdlasaskjaskjkasjckajscjaskljdasd",
+  authDomain: "dasd.das.firebaseapp.com",
+  databaseURL: "https://dasdadasdad.firebaseio.com",
+  projectId: "safasfsa",
+  storageBucket: "dsadasdasd.appspot.com",
+  messagingSenderId: "264295232442476",
+  appId: "1:2632432444:web:3668e05f9fsdfsdf34",
+  measurementId: "G-JK79J",
 };
-// const config = {
-//   apiKey: "asdlasaskjaskjkasjckajscjaskljdasd",
-//   authDomain: "dasd.das.firebaseapp.com",
-//   databaseURL: "https://dasdadasdad.firebaseio.com",
-//   projectId: "safasfsa",
-//   storageBucket: "dsadasdasd.appspot.com",
-//   messagingSenderId: "264295232442476",
-//   appId: "1:2632432444:web:3668e05f9fsdfsdf34",
-//   measurementId: "G-JK79J",
-// };
 
 firebase.initializeApp(config);
 
